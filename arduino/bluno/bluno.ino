d939fa14d544f6266273bb38e767bdecd8e8f8f0
Liza <elizaveta.sheremetyeva@gmail.com>
2017-12-27 18:40:54 +0000

    Try sending over bluetooth using button
    Didn't work yet.

--- arduino/bluno/bluno.ino
+++ arduino/bluno/bluno.ino
@@ -1,23 +1,29 @@
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);  //init the Serial
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);  //init the Serial
 }
 
-void loop()
-{
-  if (Serial.available())  {
-    while (Serial.available())
-    {
-      Serial.write(Serial.read()); //send what has been received
-      delay(100);
-    }
-    Serial.write("Arduino!");
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
 
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(500);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(500);                       // wait for a second  
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+     Serial.write("B");
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
   }
 }
-
