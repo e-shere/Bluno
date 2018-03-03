987cc983af57254270eda02f92dbd0d625f8e8cf
Liza <elizaveta.sheremetyeva@gmail.com>
2018-03-03 13:47:28 +0000

    case 3d model

--- client_socket.py
+++ client_socket.py
@@ -1,7 +1,7 @@
 import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('192.168.0.7', 8081))
+clientsocket.connect(('192.168.0.2', 8081))
 clientsocket.send('hello'.encode())
 data = clientsocket.recv(1024)
 print(data.decode())
