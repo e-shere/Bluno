51243262ee3b84d901435ab15cd49e3c8c8c0d20
Liza <elizaveta.sheremetyeva@gmail.com>
2018-03-03 18:05:48 +0000

    stop server from shutting off connection

--- server/client_socket.py
+++ server/client_socket.py
@@ -1,7 +1,7 @@
 import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('192.168.0.2', 8081))
+clientsocket.connect(('34.244.91.108', 8888))
 clientsocket.send('hello'.encode())
 data = clientsocket.recv(1024)
 print(data.decode())
--- server/server_socket.py
+++ server/server_socket.py
@@ -4,17 +4,14 @@ import socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(("", 8888))
 serversocket.listen(2)
-
+print("I am running")
+connection, address = serversocket.accept()
+print("Something connected")
 while True:
-    print("I am running")
-    connection, address = serversocket.accept()
-    print("Something connected")
     data = connection.recv(1024)
     if len(data) > 0:
         print("Received: " + data.decode())
         connection.send(data)
-    connection.close()
-    break
 
 serversocket.close()
 
