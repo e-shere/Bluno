d8f4e27c53086681db0955049fbc0fe94bcafc0c
Elizaveta Sheremetyeva <elizaveta.sheremetyeva@gmail.com>
2018-03-03 18:14:46 +0000

    Fix error on port reuse

--- server/server_socket.py
+++ server/server_socket.py
@@ -2,6 +2,7 @@ from time import sleep
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serversocket.bind(("", 8888))
 serversocket.listen(2)
 print("I am running")
