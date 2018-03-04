50f416a64db51359cf3768e01dc12f1fb8ab2883
Elizaveta Sheremetyeva <elizaveta.sheremetyeva@gmail.com>
2018-03-04 20:28:50 +0000

    forward message as it is

--- server/server_socket.py
+++ server/server_socket.py
@@ -2,31 +2,30 @@ import asyncio
 
 clients = {}
 def accept_client(client_reader, client_writer):
-    ("There is a client!")
     task = asyncio.Task(handle_client(client_reader, client_writer))
     clients[task] = (client_reader, client_writer)
 
 @asyncio.coroutine
 def handle_client(client_reader, client_writer):
-    client_writer.write("Hello\n".encode())
+    print("Client connected")
+    client_writer.write("Connected to server\n".encode())
     while True:
         line = yield from client_reader.readline()
         if line.decode() == "":
-            print("client left")
+            print("Client left")
             break
         print("Received: ", line.decode())
-        to_send= "You sent: " + line.decode()
         for client in clients:
             writer = clients[client][1]
             if writer == client_writer:
                 continue
-            writer.write(to_send.encode())
+            writer.write(line)
             yield from writer.drain()
     
 
 loop = asyncio.get_event_loop()
 f = asyncio.start_server(accept_client, host=None, port=8888)
-print("I am running")
+print("Server running")
 loop.run_until_complete(f)
 loop.run_forever()
 loop.close()
