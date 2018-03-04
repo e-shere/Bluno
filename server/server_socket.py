3541bd83335db754fdb2d1cc6b066c804be5dfe2
Liza <elizaveta.sheremetyeva@gmail.com>
2018-03-04 17:17:45 +0000

    server handles client leaving

--- server/server_socket.py
+++ server/server_socket.py
@@ -1,19 +1,23 @@
 import asyncio
 
 clients = {}
-
 def accept_client(client_reader, client_writer):
     ("There is a client!")
     task = asyncio.Task(handle_client(client_reader, client_writer))
     clients[task] = (client_reader, client_writer)
 
+@asyncio.coroutine
 def handle_client(client_reader, client_writer):
     client_writer.write("Hello\n".encode())
     while True:
         line = yield from client_reader.readline()
+        if line.decode() == "":
+            print("client left")
+            break
         print("Received: ", line.decode())
-        client_writer.write(line)
-        yield from client_writed.drain()
+        to_send= "You sent: " + line.decode()
+        client_writer.write(to_send.encode())
+        yield from client_writer.drain()
     
 
 loop = asyncio.get_event_loop()
@@ -21,5 +25,6 @@ f = asyncio.start_server(accept_client, host=None, port=8081)
 print("I am running")
 loop.run_until_complete(f)
 loop.run_forever()
+loop.close()
 
 
