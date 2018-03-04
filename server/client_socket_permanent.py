d68cb184f6bba994b61d41190b4bd611fd7e2553
Liza <elizaveta.sheremetyeva@gmail.com>
2018-03-04 14:43:15 +0000

    asynchrous client

--- server/client_socket_permanent.py
+++ server/client_socket_permanent.py
@@ -1,11 +1,28 @@
-import socket
+import asyncio
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('34.245.192.39', 8888))
+@asyncio.coroutine
+def connect(loop):
+    reader,writer = yield from asyncio.open_connection('34.245.192.39', 8888)
+    print("connected :)")
+    return reader,writer
 
-while True:
-    data = input("what shall I send? ")
-    clientsocket.send(data.encode())
-    data = clientsocket.recv(1024)
-    print(data.decode())
-    
+@asyncio.coroutine
+def receiver(reader):
+    while True:
+        response = yield from reader.read(1)
+        print(response.decode())
+        
+
+@asyncio.coroutine
+def transmitter(writer):
+    while True:
+        yield from asyncio.sleep(1)
+        writer.write("A".encode())
+        yield from writer.drain()
+
+loop = asyncio.get_event_loop()
+reader,writer  = loop.run_until_complete(connect(loop))
+tasks = [loop.create_task(receiver(reader)), loop.create_task(transmitter(writer))]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
