524ba025a18297e6157eb9456db733ed02180a3b
Liza <elizaveta.sheremetyeva@gmail.com>
2018-03-04 13:56:07 +0000

    updates server ip

--- server/client_socket_permanent.py
+++ server/client_socket_permanent.py
@@ -1,7 +1,7 @@
 import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('34.244.91.108', 8888))
+clientsocket.connect(('34.245.192.39', 8888))
 
 while True:
     data = input("what shall I send? ")
