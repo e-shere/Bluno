c8be5d048f558472b9bd462ea129fe32980607ff
Liza <elizaveta.sheremetyeva@gmail.com>
2018-03-04 15:00:58 +0000

    asynchrous client user input

--- server/client_socket_permanent.py
+++ server/client_socket_permanent.py
@@ -1,10 +1,19 @@
 import asyncio
+import sys
 
 @asyncio.coroutine
 def connect(loop):
     reader,writer = yield from asyncio.open_connection('34.245.192.39', 8888)
     print("connected :)")
     return reader,writer
+
+@asyncio.coroutine
+def std_input(loop):
+    reader = asyncio.StreamReader()
+    reader_protocol = asyncio.StreamReaderProtocol(reader)
+    print("set reader and reader protocol")
+    yield from loop.connect_read_pipe(lambda: reader_protocol, sys.stdin)
+    return reader
 
 @asyncio.coroutine
 def receiver(reader):
@@ -14,15 +23,18 @@ def receiver(reader):
         
 
 @asyncio.coroutine
-def transmitter(writer):
+def transmitter(writer,user_input):
     while True:
-        yield from asyncio.sleep(1)
-        writer.write("A".encode())
+        line = yield from user_input.readline()
+        print("read line", line.decode())
+        writer.write(line)
         yield from writer.drain()
+
 
 loop = asyncio.get_event_loop()
 reader,writer  = loop.run_until_complete(connect(loop))
-tasks = [loop.create_task(receiver(reader)), loop.create_task(transmitter(writer))]
+user_input = loop.run_until_complete(std_input(loop))
+tasks = [loop.create_task(receiver(reader)), loop.create_task(transmitter(writer,user_input))]
 loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
 
